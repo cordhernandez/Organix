b3bb2a0895a3b47b2702e3f45f779738e477249d
Cordero <cord.a.hernandez@gmail.com>
2017-11-04 18:18:20 -0700

    Adds IBOutlets to BusinessesTableViewCell

---
+++ Cells/BusinessesTableViewCell.swift
@@ -0,0 +1,21 @@
+//
+//  BusinessesTableViewCell.swift
+//  Organix
+//
+//  Created by Cordero Hernandez on 11/2/17.
+//  Copyright © 2017 Cordero Hernandez. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class BusinessesTableViewCell: UITableViewCell {
+    
+    @IBOutlet weak var businessName: UILabel!
+    @IBOutlet weak var businessImage: UIImageView!
+    @IBOutlet weak var businessRating: UILabel!
+    @IBOutlet weak var businessPrice: UILabel!
+    @IBOutlet weak var businessDistance: UILabel!
+    @IBOutlet weak var businessPhoneNumber: UILabel!
+    
+}
