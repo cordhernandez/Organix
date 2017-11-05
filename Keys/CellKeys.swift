0102c6dae336b5880517c933e9eac5187f62eb6c
Cordero <cord.a.hernandez@gmail.com>
2017-11-04 18:18:36 -0700

    Adds CellKeys class

---
+++ Keys/CellKeys.swift
@@ -0,0 +1,14 @@
+//
+//  CellKeys.swift
+//  Organix
+//
+//  Created by Cordero Hernandez on 11/2/17.
+//  Copyright © 2017 Cordero Hernandez. All rights reserved.
+//
+
+import Foundation
+
+class CellKeys {
+    
+    static let businessesCellKey = "BusinessesCell"
+}
