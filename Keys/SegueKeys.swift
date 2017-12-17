d299314634dbebcc4ed2017fdeab5c522ece2d2f
Cordero <cord.a.hernandez@gmail.com>
2017-12-16 23:01:12 -0800

    Updates SegueKeys.swift

---
+++ Keys/SegueKeys.swift
@@ -0,0 +1,14 @@
+//
+//  SegueKeys.swift
+//  Organix
+//
+//  Created by Cordero Hernandez on 12/16/17.
+//  Copyright © 2017 Cordero Hernandez. All rights reserved.
+//
+
+import Foundation
+
+class SegueKeys {
+    
+    static let mapView = "mapViewSegue"
+}
