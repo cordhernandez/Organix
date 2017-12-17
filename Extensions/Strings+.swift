065596d18572e0a78e2b7ea1214e155bef8815a2
Cordero <cord.a.hernandez@gmail.com>
2017-12-16 23:01:58 -0800

    Adds Strings+.swift Extension

---
+++ Extensions/Strings+.swift
@@ -0,0 +1,16 @@
+//
+//  Strings+.swift
+//  Organix
+//
+//  Created by Cordero Hernandez on 12/16/17.
+//  Copyright © 2017 Cordero Hernandez. All rights reserved.
+//
+
+import Foundation
+
+extension String {
+    
+    var notEmpty: Bool {
+        return !isEmpty
+    }
+}
