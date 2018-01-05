ac018f2f0c499bc6c997b3cb910cad811a1bb728
Cordero <cord.a.hernandez@gmail.com>
2018-01-04 22:58:17 -0800

    Adds WelcomeScreenDelegate

---
+++ WelcomeScreens/WelcomeScreenDelegate.swift
@@ -0,0 +1,14 @@
+//
+//  WelcomeScreenDelegate.swift
+//  Organix
+//
+//  Created by Cordero Hernandez on 1/4/18.
+//  Copyright © 2018 Cordero Hernandez. All rights reserved.
+//
+
+import Foundation
+
+protocol WelcomeScreenDelegate {
+    
+    func didDismissWelcomeScreens()
+}
